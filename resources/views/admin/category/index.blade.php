969a2ecc7af920c7f054dc831b1cab805c7705eb
imsamnang <applephagna@gmail.com>
2019-01-11 22:44:16 +0700

    show list category

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -20,9 +20,10 @@
 			<div class="row-fluid sortable">		
 				<div class="box span12">
 					<div class="box-header" data-original-title>
-						<h2><i class="halflings-icon user"></i><span class="break"></span>Members</h2>
+						<h2><i class="halflings-icon user"></i><span class="break"></span>Members</h2>&nbsp;
+						<span><a href="{{route('admin.category.create')}}" class="label label-success"><i class="halflings-icon white plus"></i>Add Category</a></span>
 						<div class="box-icon">
-							<a href="#" class="btn-setting"><i class="halflings-icon wrench"></i></a>
+							<a href="#" class="btn-setting"><i class="halflings-icon plus"></i></a>
 							<a href="#" class="btn-minimize"><i class="halflings-icon chevron-up"></i></a>
 							<a href="#" class="btn-close"><i class="halflings-icon remove"></i></a>
 						</div>
@@ -31,25 +32,36 @@
 						<table class="table table-striped table-bordered bootstrap-datatable datatable">
 						  <thead>
 							  <tr>
-								  <th>Username</th>
-								  <th>Date registered</th>
-								  <th>Role</th>
+								  <th>ID</th>
+								  <th>Category Name</th>
+								  <th>Description</th>
 								  <th>Status</th>
 								  <th>Actions</th>
 							  </tr>
 						  </thead>   
 						  <tbody>
+						  	@foreach ($categories as $key => $row)
 							<tr>
-								<td>Dennis Ji</td>
-								<td class="center">2012/01/01</td>
-								<td class="center">Member</td>
+								<td>{{++$key}}</td>
+								<td class="center">{{$row->category_name}}</td>
+								<td class="center">{!!$row->category_description!!}</td>
 								<td class="center">
-									<span class="label label-success">Active</span>
+									@if ($row->publication_status==1)
+										<span class="label label-success">Publish</span>
+									@else
+										<span class="label label-info">Unpublish</span>
+									@endif
 								</td>
 								<td class="center">
-									<a class="btn btn-success" href="#">
-										<i class="halflings-icon white zoom-in"></i>  
-									</a>
+									@if ($row->publication_status==1)
+										<a class="btn btn-danger" href="#">
+											<i class="halflings-icon white thumbs-down"></i>  
+										</a>
+									@else
+										<a class="btn btn-success" href="#">
+											<i class="halflings-icon white thumbs-up"></i>
+										</a>
+									@endif
 									<a class="btn btn-info" href="#">
 										<i class="halflings-icon white edit"></i>  
 									</a>
@@ -59,6 +71,7 @@
 								</td>
 								</td>
 							</tr>
+						  	@endforeach
 						  </tbody>
 					  </table>            
 					</div>
